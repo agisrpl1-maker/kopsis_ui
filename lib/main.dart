8f729c5284b005c656072e979c6335e2ea21350d
windaamalya <windaxpplg1@gmail.com>
2026-08-21 07:40:02 +0700

    LKM-3 kartu barang

--- lib/main.dart
+++ lib/main.dart
@@ -1,31 +1,56 @@
 import 'package:flutter/material.dart';
-void main() => runApp(const MyApp());
+
+void main() => runApp(MyApp());
 
 class MyApp extends StatelessWidget {
-  const MyApp ({super.key});
+  MyApp({super.key});
+  // Variabel stok
+  int stok = 40;
 
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       home: Scaffold(
-        appBar: AppBar(title: const Text('Koperasi Sekolah')),
-        body: Container(
-          padding: const EdgeInsets.all(16),
-          child: Column(
-            crossAxisAlignment: CrossAxisAlignment.start,
-            children: [
-              const Text('Buku Tulis'),
-              const SizedBox(height: 8,),
-              Row(
-                mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                children: const[
-                  Text('Anggota: 3.000'),
-                  Text('Umum: 3.500'),
-                ],
-              )
-            ],
+        appBar: AppBar(
+          title: const Text('Koperasi Sekolah'),
+        ),
+        body: Card(
+          margin: const EdgeInsets.all(12),
+          child: ListTile(
+            contentPadding: const EdgeInsets.symmetric(
+              horizontal: 12,
+              vertical: 4,
+            ),
+            leading: const Icon(Icons.inventory_2),
+            title: const Text(
+              'Buku Tulis',
+              style: TextStyle(
+                fontSize: 18,
+                fontWeight: FontWeight.bold,
+              ),
+            ),
+            subtitle: const Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              mainAxisSize: MainAxisSize.min,
+              children: [
+                Text('Anggota Rp3.000 | Umum Rp3.500'),
+                Text(
+                  'Tersedia',
+                  style: TextStyle(
+                    fontWeight: FontWeight.bold,
+                  ),
+                ),
+              ],
+            ),
+            trailing: Text(
+              'Stok ' + stok.toString(),
+              style: TextStyle(
+                color: stok == 40 ? Colors.red : Colors.black,
+                fontWeight: FontWeight.bold,
+              ),
+            ),
           ),
-        )
+        ),
       ),
     );
   }
